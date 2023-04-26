7512b33873bb230407ba4d29d75988db09f11357
matheusmcz <matheusmczvieira@gmail.com>
2023-04-26 09:42:15 -0300

    chore: History component

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,54 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu historico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="green">Concluido</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="yellow">Em andamento</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="green">Concluido</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="red">Interrompido</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="green">Concluido</Status>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
+}
